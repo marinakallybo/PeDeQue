cd89854f327905d46fa7317d91825fec7165d9d9
marinakallybo <marinakally@gmail.com>
2026-06-22 16:25:03 -0300

    Alteração do nome do projeto para PéDeQuê?

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agroseed.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plantou.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
